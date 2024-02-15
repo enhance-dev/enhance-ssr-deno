abeb2c762f14f437f136e70b432c7482b80436d4
brianleroux <b@brian.io>
2024-02-15 09:55:22 -0800

    better tests and readme

--- test.js
+++ test.js
@@ -4,3 +4,32 @@ import enhance from './mod.js'
 Deno.test('enhancer exists', () => {
   assert (enhance, 'exists')
 })
+
+function Header ({html}) {
+  return html`<header>header here</header>`
+}
+
+function Footer ({html}) {
+  return html`<footer>footer here</footer>`
+}
+
+function Layout ({html}) {
+  return html`<my-header></my-header><slot/><my-footer></my-footer>`
+}
+
+function Page ({html}) {
+  return 
+}
+
+Deno.test('can render', () => {
+  let html = enhance({
+    elements: {
+      'my-header': Header,
+      'my-footer': Footer,
+      'my-layout': Layout
+    }
+  })
+  let result = html`<my-layout><b>hi</b></my-layout>`
+  assert(result, 'ssr!')
+  console.log(result)
+})
